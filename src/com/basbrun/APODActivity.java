ab4c93eb217e84e20ed0e4d73d60957a7838e597
Philippe Chrtien <philippe.chretien@gmail.com>
2012-07-10 20:36:57 -0400

    Fixed the python style comments in .java files ...

--- src/com/basbrun/APODActivity.java
+++ src/com/basbrun/APODActivity.java
@@ -1,30 +1,29 @@
-## APOD ##
-#
-# This project is an Android client to visualize the Astronomy Picture Of The
-# Day (APOD) published by NASA at http://apod.nasa.gov/apod/. This project is
-# free of charges and have been developed with the objective to learn Android
-# programmation.
-#
-# Copyright (C) 2012  Philippe Chretien
-#
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License Version 2
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-#
-# You will find the latest version of this code at the following address:
-# http://github.com/pchretien
-#
-# You can contact me at the following email address:
-# philippe.chretien at gmail.com
-
+//// APOD ////
+//
+// This project is an Android client to visualize the Astronomy Picture Of The
+// Day (APOD) published by NASA at http://apod.nasa.gov/apod/. This project is
+// free of charges and have been developed with the objective to learn Android
+// programmation.
+//
+// Copyright (C) 2012  Philippe Chretien
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License Version 2
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+//
+// You will find the latest version of this code at the following address:
+// http://github.com/pchretien
+//
+// You can contact me at the following email address:
+// philippe.chretien at gmail.com
 
 package com.basbrun;
 
